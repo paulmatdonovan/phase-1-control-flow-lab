1f60fce68c66fa3930f39821a188b726fd809675
Paul Donovan <paul.m.donovan@outlook.com>
2024-02-10 10:25:24 +1100

    addsJSTests

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue) {
   // Write your code here!
-}
+  let result;
+  if (someValue <= 400) {
+    result = 'This one is on me!'
+  }
+  else if (someValue < 2000) {
+    result = 'That will be twenty bucks.'
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  else if (someValue > 2500) {
+    result = 'No can do.'
+
+  }
+  else if (someValue > 2000) {
+    result = 'I will gladly take your thirty bucks.'
+  }
+  return result
+
+
 }
+scuberGreetingForFeet()
+
+
+function ternaryCheckCity(city) {
+  // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+
+
+}
+ternaryCheckCity()
+
+function switchOnCharmFromTip(n) {
+  // Write your code here!
+
+
+
+  switch (n) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+
+    default:
+      return 'Bye.';
+      break;
+  }
+}
+switchOnCharmFromTip()
